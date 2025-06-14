577d88efab06c617f3e310f4f746623c5e7ac2c3
Luis David <luisdavidmirandav12@gmail.com>
2025-06-14 16:32:58 -0600

    First commit

---
+++ internal/app/app.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"log"
+	"os"
+	"fmt"
+	"net/http"
+)
+type Application struct {
+	Logger *log.Logger
+}
+
+func NewApplication() (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	app := &Application{
+		Logger: logger,
+	}
+	return app, nil
+}
+
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Status is avaliable\n")
+}
